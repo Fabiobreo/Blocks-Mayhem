4259d9fcf2882f2cc890d0c0e7a66bcd2cf847ca
Fabio Brea <fabio.brea@gmail.com>
2021-12-14 09:07:18 +0100

    Committed local project

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //Configuration parameters
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites;
+
+    // Cached reference
+    Level level;
+
+    // State variables
+    int timesHit; 
+
+    private void Start()
+    {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Breakable")
+        {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        if (hitSprites[timesHit - 1] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[timesHit - 1];
+        }
+        else
+        {
+            Debug.LogError("Missing sprite from " + gameObject.name);
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        PlayBlockSFX();
+        Destroy(gameObject);
+        level.BlockDestroyed();
+        FindObjectOfType<GameSession>().AddToScore();
+        TriggerSparklesVFX();
+    }
+
+    private void PlayBlockSFX()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
+}
